4ee323ce02b60f3b58e1cf3fe4fc616c99d64b77
Viktor Speranskyy <vencedor@ukr.net>
2019-08-22 17:55:38 +0300

    v.2

--- MySQL/MainWindow.xaml.cs
+++ MySQL/MainWindow.xaml.cs
@@ -1,19 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using MySql.Data.MySqlClient;
 
 namespace MySQL
 {
@@ -28,46 +17,68 @@ namespace MySQL
             InitializeComponent();
 
             // строка подключения к БД
-            // string connStr = "server=https://remotemysql.com;port=3306;user=Bty2hZRByP;database=Bty2hZRByP;password=YvfJSreBZB";
-            string connStr = "server=127.0.0.1;port=3306;user=root;password=password;database=test;";
+            string connStr = "server=192.168.1.152;port=3309;user=itschool;database=itschool;";
+            //            string connStr = "server=127.0.0.1;port=3306;user=root;password=password;database=test;";
             // создаём объект для подключения к БД
             conn = new MySqlConnection(connStr);
+            RefreshAll();
+        }
 
+        public void RefreshAll()
+        {
+            gridView.ItemsSource = Select().DefaultView;
+
+            cmbUsers.ItemsSource = Select().DefaultView;
+            cmbUsers.DisplayMemberPath = "login";
+            cmbUsers.SelectedValuePath = "id";
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
-            Select();            
-
+            ShowDataInRichTextBox();
         }
 
+        public void Update(string query = "")
+        {
+            if (query.Equals(""))
+            {
+                Random r = new Random();
+                int n = 10 + r.Next(100);
+                query = $"INSERT INTO users VALUES (NULL, 'user{n}', 'user{n}', 'user{n}', NOW())";
+            }
 
+            // устанавливаем соединение с БД
+            if (conn.State != ConnectionState.Open)
+                conn.Open();
 
-        public void Update()
+            //            MessageBox.Show(query);
+            MySqlCommand MyCommand = new MySqlCommand(query, conn);
+            try
+            {
+                MyCommand.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+            RefreshAll();
+
+            conn.Close();
+        }
+
+        public DataTable Select(string query = "SELECT id, name, login, password, DATE_FORMAT(regdate, '%d.%m.%Y  %T') AS regdate FROM users")
         {
             // устанавливаем соединение с БД
             if (conn.State != ConnectionState.Open)
                 conn.Open();
 
-            MySqlCommand MyCommand = new MySqlCommand("INSERT INTO users VALUES (NULL, 'user111', 'user111', 'user111')", conn);
-            MyCommand.ExecuteNonQuery();
-            conn.Close();
-        }
-
-        public void Select()
-        {
-            // устанавливаем соединение с БД
-            conn.Open();
-
-            // Строка запроса
-            string sql = "SELECT * FROM users";
             // объект для выполнения SQL-запроса
-            MySqlCommand sqlCom = new MySqlCommand(sql, conn);
+            MySqlCommand sqlCom = new MySqlCommand(query, conn);
 
             // выполняем запрос и получаем ответ
-            string name = sqlCom.ExecuteScalar().ToString();
-            MessageBox.Show(name);
+            //            string name = sqlCom.ExecuteScalar().ToString();
+            //            MessageBox.Show(name);
 
             // выполняем запрос и получаем ответ
             sqlCom.ExecuteNonQuery();
@@ -75,7 +86,20 @@ namespace MySQL
             DataTable dt = new DataTable();
             dataAdapter.Fill(dt);
 
-            var myData = dt.Select();
+            return dt;
+        }
+
+        public void ShowDataInRichTextBox()
+        {
+            // устанавливаем соединение с БД
+            if (conn.State != ConnectionState.Open)
+                conn.Open();
+
+            string query = "SELECT * FROM users";
+            richTextBox1.Items.Clear();
+
+            richTextBox1.Items.Add("1 вариант\n");
+            var myData = Select(query).Select();
             for (int i = 0; i < myData.Length; i++)
             {
                 for (int j = 0; j < myData[i].ItemArray.Length; j++)
@@ -83,23 +107,75 @@ namespace MySQL
                 richTextBox1.Items.Add("\n");
             }
 
-            MySqlDataReader MyDataReader;
-            MyDataReader = sqlCom.ExecuteReader();
-            while (MyDataReader.Read())
-            {
-                int id = MyDataReader.GetInt32(0); //Получаем целое число
-                string name1 = MyDataReader.GetString(1); //Получаем строку
 
-                richTextBox1.Items.Add(id + "\t" + MyDataReader.GetString(1) + "\t" + MyDataReader.GetString(2));
+            richTextBox1.Items.Add("-----------------\n 2 вариант\n");
+
+
+            MySqlDataReader MyDataReader = null;
+            MySqlCommand sqlCom = null;
+
+            try
+            {
+                sqlCom = new MySqlCommand(query, conn);
+                MyDataReader = sqlCom.ExecuteReader();
             }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+            if (MyDataReader != null && MyDataReader.HasRows)
+                while (MyDataReader.Read())
+                {
+                    int id = MyDataReader.GetInt32(0); //Получаем целое число
+                    string name1 = MyDataReader.GetString(1); //Получаем строку
+
+                    richTextBox1.Items.Add(id + "\t" + MyDataReader.GetString(1) + "\t" + MyDataReader.GetString(2));
+                }
 
             // закрываем соединение с БД
             conn.Close();
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void btnUpdateClick(object sender, RoutedEventArgs e)
         {
             Update();
+        }
+
+        private void GridView_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            string query = "";
+            DataRowView dataRow = (DataRowView)gridView.SelectedItem;
+            MessageBox.Show(gridView.SelectedIndex.ToString());
+            if (gridView.SelectedIndex < gridView.Items.Count - 2)
+            {
+                //                int index = gridView.CurrentCell.Column.DisplayIndex;
+                string columnName = gridView.CurrentCell.Column.Header.ToString();
+                string newValue = ((TextBox)e.EditingElement).Text;
+                int id = (int)dataRow.Row.ItemArray[0];
+
+                query = "UPDATE users SET " + columnName + "=\"" + newValue + "\" WHERE id=" + id;
+            }
+            else
+            {
+                for (int i = 1; i < dataRow.Row.Table.Columns.Count; i++)
+                    if (gridView.SelectedItems[i].ToString().Equals(""))
+                        return;
+
+                query = $"INSERT INTO users VALUES (NULL, {dataRow.Row.ItemArray[1]}, {dataRow.Row.ItemArray[2]}, {dataRow.Row.ItemArray[3]}, NOW());";
+                MessageBox.Show(query);
+            }
+            Update(query);
+        }
+
+        private void GridView_ContextMenuOpening(object sender, ContextMenuEventArgs e)
+        {
+            mainWindow.Title = e.Source.ToString();
+        }
+
+        private void CmbUsers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            gridView.ItemsSource = Select("SELECT * FROM users WHERE id = " + ((ComboBox)sender).SelectedValue.ToString()).DefaultView;
         }
     }
 }

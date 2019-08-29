2caf1f02e46e2f61d95c90992ad20a5311c1fc60
Viktor Speranskyy <vencedor@ukr.net>
2019-08-29 17:50:07 +0300

    upd.2

--- MySQL/App.xaml.cs
+++ MySQL/App.xaml.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
 using System.Windows;
 
 namespace MySQL
